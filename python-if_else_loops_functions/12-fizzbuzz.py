42f4c5bcaf65e3addc111a0c2593c13c698d03fc
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-02 01:47:23 -0800

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
-fizzbuzz = __import__(' 12-fizzbuzz ').fizzbuzz
+#!/usr/bin/python
 def fizzbuzz():
-    print("")
+    for i in range(1, 100):
+        if i % 3 == 0:
+            print("fizz")
+            if i % 5 == 0:
+                print("buzz")
+fizzbuzz()
