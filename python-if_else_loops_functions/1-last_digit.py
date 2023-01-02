9b55ca9eef1bac36ac4052eafa9bf4abef781688
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-01 21:41:41 -0800

    1-last_digit.py

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if last digit > 5:
+    print("{} the string and is greater than 5".format(number))
+elif last digit == 0:
+    print("{} the string and is 0".format(number))
+else:
+    print("{} the string and is lessthan 6 and not 0".format(number))
+
