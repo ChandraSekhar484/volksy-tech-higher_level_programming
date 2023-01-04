12033e388fd7b0c93845a4dd7c1cc3345ae4bcde
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-04 01:20:02 -0800

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -22,5 +22,7 @@ def main():
         print('{} * {} = {}'.format(a, b, mul(a, b)))
     elif argv[2] == '/':
         print('{} / {} = {}'.format(a, b, div(a, b)))
-if __name__ == "__main__":
+  
+
+  if __name__ == "__main__":
     main()
