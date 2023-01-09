3463f05b918cef70f93cfb0540dd4ca2f159b762
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-08 20:02:34 -0800

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for k in (a_dictionary):
-        return ({k: a_dictionary[k] * 2})
+    new_dictionary = {}
+    for i in a_dictionary:
+        new_dictionary[i] = a_dictionary[i] * 2
+    return new_dictionary
