debe0f8ea5c2f35036710671efd4d717c51cc3c0
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-10 03:58:26 -0800

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square"""
+
+
+class Square:
+    """square"""
+    def size(self):
+    def __init__(self, value):
+        """data"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
