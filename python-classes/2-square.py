18fb06c6191f56cb81256b8514dbe84c891fae2b
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-10 03:44:28 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,6 @@ class Square:
     """square"""
     def __init__(self, size=0):
         """data"""
-
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
