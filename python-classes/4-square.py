ff785d7465460922af7fb0bfac27b44f734731ab
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-10 20:45:55 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Represents a square"""
+
+
 class Square:
-    """Represents a square"""
-
-
+    """"square"""
     def __init__(self, size=0):
         """Initializes the data"""
         self.size = size
