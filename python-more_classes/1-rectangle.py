86128588b16bc2c4881d48ff49e57511b4c37676
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-16 20:24:19 -0800

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-class Rectangle:
     """Represents a rectangle."""
+
+    class Rectangle:
+        """Rectangle"""
     def __init__(self, width=0, height=0):
         """Initialize the data."""
         self.width = width
