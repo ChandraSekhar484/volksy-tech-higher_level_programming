68cc3653c0b40e5b11891b8640c2ccfc90b625bd
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 02:12:04 -0800

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """here we are using append function to the file"""
     with open(filename, "a", encoding="utf-8") as f:
-        print(f.append(), end="")
+        return f.write(text)
