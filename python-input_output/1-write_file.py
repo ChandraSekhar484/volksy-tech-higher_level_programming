84cb9de4f093729d81f7a80f979cb577286765ab
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 01:48:31 -0800

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""It defines a file write_function"""
+
+
+def write_file(filename="", text=""):
+    """writing_file function"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.write(), end="")
