bc344e00c683796d528ce81bedb042d97562a5bb
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 01:30:50 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""""defines read file name"""
+
+
 def read_file(filename=""):
+    """"defining file name with read operation"""
     with open("0-read_file.txt","r")as f:
         f.read_file("0-read_file.txt")
