8c17e1a8c65d8d26892e0d93a1899b6e1c2831af
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 03:29:02 -0800

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import json
 """"JSON representation of an object string"""
-import JSON
 
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object string"""
-    return JSON.dumps(my_obj)
+    return json.dumps(my_obj)
