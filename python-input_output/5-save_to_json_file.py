6b5af1836af5d5489dd627a399ab79b8136c1a18
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 20:44:44 -0800

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+"""Save Object to a file"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ function that writes an Object to a text file, using a JSON representation"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
