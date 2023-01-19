6a85ad3a7bbc5f78e6970928d5fd5346f4d2fb92
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-18 22:18:43 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,13 +4,11 @@
 
 class Student():
     """class to define student"""
-
     def __init__(self, first_name, last_name, age):
         """initializes student instances"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
     def to_json(self, attrs=None):
         """returns dictionary descript for JSON serialization"""
         if type(attrs) is list:
