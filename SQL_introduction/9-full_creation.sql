c89c41449d63c2b3ba768399ca04c0a6f3613669
brightcomg <chandrasekhars@brightcomgroup.com>
2023-01-29 22:54:31 -0800

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- a script that creates a table and add multiples rows
-CREATE TABLE if not exists second_table(id INT,name VARCHAR(256),score INT);
+CREATE TABLE if not exists SHOW CREATE TABLE second_table(id INT,name VARCHAR(256),score INT);
 INSERT INTO second_table(id,name,score)VALUES(1,"John",10);
 INSERT INTO second_table(id,name,score)VALUES(2,"Alex",3);
 INSERT INTO second_table(id,name,score)VALUES(3."Bob",14);
