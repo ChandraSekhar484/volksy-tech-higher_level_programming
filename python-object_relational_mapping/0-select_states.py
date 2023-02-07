b3fcabc7bbd7b0f9f3b7a1fda6eb5791468cee9b
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-07 00:49:39 -0800

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
--- Lists all states from the database hbtn_0e_0_usa
-import sys
-import MySQLdb
-
+"""
+lists all states from the database in ascending order
+using MySQLdb
+"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
