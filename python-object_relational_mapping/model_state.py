e4d9cb7e3180c1fbe2d7ca8802dc32e3616e601c
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-08 22:43:12 -0800

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
-
-
 import sys
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 """ python file that contains the class definition of a State and an instance Base = declarative_base()"""
+
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
