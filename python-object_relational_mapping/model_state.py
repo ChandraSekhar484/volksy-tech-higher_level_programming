d6833b019961cfd387351f0919d34249acfce8ec
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-08 22:34:42 -0800

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""
-lists all cities from the database in ascending order
-matching state name passed in as argument using MySQLdb
-"""
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("""SELECT c.name FROM cities AS c \
-                INNER JOIN states AS s ON c.state_id = s.id\
-                WHERE s.name = '{}' \
-                ORDER BY c.id ASC""".format(argv[4]))
-    rows = cur.fetchall()
-    for i, row in enumerate(rows, start=0):
-        if i != 0:
-            print(", ", end="")
-        print(row[0], end="")
-    print("")
-    cur.close()
-    db.close()
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
