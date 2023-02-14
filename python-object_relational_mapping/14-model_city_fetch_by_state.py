6455fa9c0765c5581f4645a15d677e9115226024
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-13 21:54:34 -0800

    14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-Script to list all states in the states table.
-"""
-from model_city import City
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""A script that deletes State objects with a name containing the letter a"""
+
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for state_name, city_id, city_name in session\
-            .query(State.name, City.id, City.name)\
-            .filter(City.state_id == State.id)\
-            .order_by(City.id):
-        print(f"{state_name}: ({city_id}) {city_name}")
+    for row in session.query(State, City).join(City).order_by(City.id):
+        print("{:}: ({:}) {:}".format(row[0].name, row[1].id, row[1].name))
+    session.commit()
+    session.close()
