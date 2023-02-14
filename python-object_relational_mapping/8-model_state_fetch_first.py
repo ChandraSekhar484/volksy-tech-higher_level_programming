0fe6ae7b6f08a01fa73c8b75a7f14add61b7fde6
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-13 21:20:54 -0800

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-prints the first State object from the database without fetch all states
-using SQLAlchemy and importing State and Base from model_state
+Script to list all states in the states table.
 """
-from sys import argv
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        instance = session.query(State).first()
-        if instance is not None:
-                print("{}: {}".format(instance.id, instance.name))
-        else:
-                print("Nothing")
-        session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
