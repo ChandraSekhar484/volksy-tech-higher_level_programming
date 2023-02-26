38f4556895bcb42587acf9e463b967ab19d19c9b
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-25 23:11:41 -0800

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-// script that displays the status code of a GET request
 const request = require('request');
-const myArgs = process.argv.slice(2);
-request(myArgs[0], function (err, response)) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
-};
+
+request(process.argv[2], function (error, response) {
+  if (error) throw error;
+  console.log('code: ' + response.statusCode);
+});
