d9080ce4d4a023be22c0fd667c1fb5bf6983be01
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-25 23:17:21 -0800

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+// script that displays the title of the Star Wars movie using API
 const request = require('request');
-
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) throw error;
-  console.log(JSON.parse(body).title);
+const myArgs = process.argv.slice(2);
+const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+request(URLstring, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
