45ce74eeeed7a28602be7e7cea6b86f456e1ae5a
brightcomg <chandrasekhars@brightcomgroup.com>
2023-02-26 22:23:41 -0800

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,11 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie
-const request = require('request');
-const myArgs = process.argv.slice(2);
-const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
-function characterFunc (character, i = 0, stop) {
-  if (i === stop) {
-    return;
+
+const oldDict = require('./101-data.js').dict;
+const newDict = {};
+Object.keys(oldDict).forEach(function (key) {
+  if (newDict[oldDict[key]] === undefined) {
+    newDict[oldDict[key]] = [];
   }
-  const URLcharacter = character[i];
-  request(URLcharacter, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-    }
-  });
-  i++;
-  characterFunc(character, i, stop);
-}
-request(URLstring, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const character = JSON.parse(body).characters;
-    characterFunc(character, 0, character.length);
-  }
+  newDict[oldDict[key]].push(key);
 });
+console.log(newDict);
